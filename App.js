9bdc77d0c82422eec77a1bc27e63ac9f43008151
Alfredo Silva <62573260+alfredsilvasilva@users.noreply.github.com>
2023-08-03 22:25:52 -0300

    Primeiro commit
    projeto app reactnative

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, SafeAreaView, View } from 'react-native';
+import Cesta from './src/telas/Cesta';
+
+
+
+export default function App() {
+  return (
+
+    <SafeAreaView>
+      <StatusBar />
+      <Cesta />
+    </SafeAreaView>
+  );
+}
